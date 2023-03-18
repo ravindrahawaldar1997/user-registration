a4e7fd919925fd34251b10c4f8af854e15678957
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2023-03-18 18:23:44 +0530

    Added: program to validate email sapmles

--- src/main/java/com/bridgelabz/user_registration/UserRegistrationMain.java
+++ src/main/java/com/bridgelabz/user_registration/UserRegistrationMain.java
@@ -11,5 +11,20 @@ public class UserRegistrationMain {
         userRegistration.validatePasswordOneUpperCase("RavindraAZDFgjhj");
         userRegistration.validatePasswordOneNumeric("rahgvHGF12321wsadf");
         userRegistration.validatePasswordOneSpecialChar("FDhgsavch13@");
+        
+        String[] validEmail = {"abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com",
+                "abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au",
+                "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com"};
+        String[] invalidEmail = {"abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com",
+                "abc123@.com.com", ".abc@abc.com", "abc()*@gmail.com", "abc@%*.com",
+                "abc..2002@gmail.com", "abc.@gmail.com", "abc@abc@gmail.com",
+                "abc@gmail.com.1a", "abc@gmail.com.aa.au"};
+        for (String email: validEmail) {
+            userRegistration.validateEmail(email);
+        }
+
+        for (String email : invalidEmail) {
+            userRegistration.validateEmail(email);
+        }
     }
 }
